0853f3d2aa31f972cb1d422183e3c1e70914ee81
TC <chengtian372@gmail.com>
2020-02-14 21:51:55 -0500

    Update authentication.py
    user login logout register

--- server/app/authentication.py
+++ server/app/authentication.py
@@ -1,8 +1,9 @@
 from flask import Flask, escape, request
 import mysql.connector
-from db import get_db
 from flask.helpers import flash
-
+from flask import session
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -17,9 +18,27 @@ def hello():
 
 @app.route("/login", methods=("GET", "POST"))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = db1.cursor()
 
+        error = None
+        db.execute('SELECT * FROM user WHERE Username = %s', (username,))
+        user = db.fetchone()
 
-    return "login successful"
+        if user is None:
+            error =  'Incorrect username.'
+        elif not check_password_hash(user[2], password):
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user[0]     
+        
+            return "login successful"
+        else:
+            return error
 
 
 @app.route("/register", methods=("GET", "POST"))
@@ -43,14 +62,23 @@ def register():
 
         else:
             db.execute(
-                "INSERT INTO user (username, password) VALUES (%s, %s,%s,%s)",
-                (username, password,gender,birthday),
+                "INSERT INTO user (username, password,Gendar, Birthday) VALUES (%s, %s,%s,%s)",
+                (username, generate_password_hash(password),gender,birthday),
             )
             db1.commit()
 
             return "successful"
             
 
+@app.route("/logout",methods=("GET", "POST"))
+def logout():
+    """Clear the current session, including the stored user id."""
+    session.clear()
+    return "log out"
+
+
+
 if __name__ == '__main__':
+    app.secret_key = 'my secret key'
     app.debug = True
     app.run()
