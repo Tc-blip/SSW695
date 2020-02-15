142f62717eea167607f83951a626abb47a260a1a
TC <chengtian372@gmail.com>
2020-02-14 19:16:59 -0500

    finish register

--- demo/server_to_mysql/test.py
+++ demo/server_to_mysql/test.py
@@ -5,18 +5,43 @@ cnx = mysql.connector.connect(user='root', password='tiancheng',
                               database='mydb')
 
 #cnx = mysql.connector.connect(user='root', database='mydb')
-cursor = cnx.cursor()
+
+db = cnx.cursor()
+
+username = 'yukaittt'
+password = 'chengle'
+db.execute("SELECT Username FROM user WHERE Username = %s", (username,))
+res = db.fetchone()
+if (res is not None ):
+   print("User {0} is already registered.".format(username))
+
+else:
+    db.execute(
+        "INSERT INTO user (username, password) VALUES (%s, %s)",
+        (username, password),
+    )
+    cnx.commit()
+
+    print( "successful")
 
 
+'''
 add_employee = ("INSERT INTO user "
                "(UserId, Username, Password, Gendar) "
-               "VALUES (10001, 'yukait', 'sdf1234', 'M')")
-
-cursor.execute(add_employee)
+               "VALUES (10002, 'tc', 'sdf1234', 'M')")
 
 
+
+search_employee = ("SELECT * from user where Username = %s ")
+
+cursor.execute(search_employee,('yukait',))
+
+myresult = cursor.fetchall()
+
+print(myresult)
 
 cnx.commit()
 
 cursor.close()
 cnx.close()
+'''
---
+++ server/app/authentication.py
@@ -0,0 +1,48 @@
+from flask import Flask, escape, request
+import mysql.connector
+from db import get_db
+from flask.helpers import flash
+
+
+app = Flask(__name__)
+
+db1 = mysql.connector.connect(user='root', password='tiancheng',
+                        host='127.0.0.1',
+                        database='mydb')
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
+@app.route("/register", methods=("GET", "POST"))
+def register():
+    """Register a new user.
+    Validates that the username is not already taken. Hashes the
+    password for security.
+    """
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db = db1.cursor()
+        
+        db.execute("SELECT Username FROM user WHERE Username = %s", (username,))
+        res = db.fetchone()
+        if (res is not None ):
+            return "User {0} is already registered.".format(username)
+
+        else:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (%s, %s)",
+                (username, password),
+            )
+            db1.commit()
+
+            return "successful"
+            
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
