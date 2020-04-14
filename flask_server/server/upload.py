b5350a72aca6a51c9db7cdf9e8d8d85b2835e057
TC <chengtian372@gmail.com>
2020-04-14 07:36:31 -0400

    add upload api

---
+++ flask_server/server/upload.py
@@ -0,0 +1,36 @@
+import os
+import urllib.request
+from flask import (
+    Blueprint, flash, g, redirect, jsonify, request, session, url_for,current_app
+)
+from werkzeug.utils import secure_filename
+bp = Blueprint('upload', __name__, url_prefix='/upload')
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/file-upload', methods=("GET", "POST"))
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+		resp.status_code = 400
+		return resp
